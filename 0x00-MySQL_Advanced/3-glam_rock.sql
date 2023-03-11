abbeacb7eec2f9422d54822ecfcb6e2a9df2ccf1
andrew21-mch <nfonandrew73@gmail.com>
2023-03-11 19:34:39 +0100

    update glam rock task 3.sql

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -2,5 +2,7 @@
 -- Column names must be: band_name and lifespan (in years)
 -- use attributes formed and split for computing the lifespan
 
-SELECT band_name, COALESCE(split, 2020) - formed as lifespan FROM
-metal_bands WHERE style 
+SELECT band_name AS band_name, IFNULL(split, 2020) - IFNULL(formed, 0) AS lifespan
+FROM metal_bands
+WHERE style LIKE '%Glam rock%'
+ORDER BY lifespan DESC;
