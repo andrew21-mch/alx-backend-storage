5cc429d25b672da16cd41ecc27082b9e6955c238
andrew21-mch <nfonandrew73@gmail.com>
2023-03-11 19:33:42 +0100

    create procedure

--- 0x00-MySQL_Advanced/100-init.sql
+++ 0x00-MySQL_Advanced/100-init.sql
@@ -1,47 +1,17 @@
--- Initial
-DROP TABLE IF EXISTS corrections;
-DROP TABLE IF EXISTS users;
-DROP TABLE IF EXISTS projects;
-
-CREATE TABLE IF NOT EXISTS users (
-    id int not null AUTO_INCREMENT,
-    name varchar(255) not null,
-    average_score float default 0,
-    PRIMARY KEY (id)
-);
-
-CREATE TABLE IF NOT EXISTS projects (
-    id int not null AUTO_INCREMENT,
-    name varchar(255) not null,
-    weight int default 1,
-    PRIMARY KEY (id)
-);
-
-CREATE TABLE IF NOT EXISTS corrections (
-    user_id int not null,
-    project_id int not null,
-    score float default 0,
-    KEY `user_id` (`user_id`),
-    KEY `project_id` (`project_id`),
-    CONSTRAINT fk_user_id FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE,
-    CONSTRAINT fk_project_id FOREIGN KEY (`project_id`) REFERENCES `projects` (`id`) ON DELETE CASCADE
-);
-
-INSERT INTO users (name) VALUES ("Bob");
-SET @user_bob = LAST_INSERT_ID();
-
-INSERT INTO users (name) VALUES ("Jeanne");
-SET @user_jeanne = LAST_INSERT_ID();
-
-INSERT INTO projects (name, weight) VALUES ("C is fun", 1);
-SET @project_c = LAST_INSERT_ID();
-
-INSERT INTO projects (name, weight) VALUES ("Python is cool", 2);
-SET @project_py = LAST_INSERT_ID();
-
-
-INSERT INTO corrections (user_id, project_id, score) VALUES (@user_bob, @project_c, 80);
-INSERT INTO corrections (user_id, project_id, score) VALUES (@user_bob, @project_py, 96);
-
-INSERT INTO corrections (user_id, project_id, score) VALUES (@user_jeanne, @project_c, 91);
-INSERT INTO corrections (user_id, project_id, score) VALUES (@user_jeanne, @project_py, 73);
+-- creates a stored procedure ComputeAverageWeightedScoreForUser that computes and store the average weighted score for a student
+DROP PROCEDURE IF EXISTS ComputeAverageWeightedScoreForUser;
+DELIMITER $$
+CREATE PROCEDURE ComputeAverageWeightedScoreForUser(
+    user_id INT
+)
+BEGIN
+    DECLARE w_avg_score FLOAT;
+    SET w_avg_score = (SELECT SUM(score * weight) / SUM(weight) 
+                        FROM users AS U 
+                        JOIN corrections as C ON U.id=C.user_id 
+                        JOIN projects AS P ON C.project_id=P.id 
+                        WHERE U.id=user_id);
+    UPDATE users SET average_score = w_avg_score WHERE id=user_id;
+END
+$$
+DELIMITER ;
